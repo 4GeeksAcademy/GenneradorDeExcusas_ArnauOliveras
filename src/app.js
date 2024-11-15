ca8f3019766e9794d0f3828a8273f0218f0eef53
ArnauOliveras <77742762+ArnauOliveras@users.noreply.github.com>
2024-11-15 17:58:56 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomWho = getRandomElement(who);
+  let randomAction = getRandomElement(action);
+  let randomWhat = getRandomElement(what);
+  let randomWhen = getRandomElement(when);
+
+  let randomSentence = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  let element = document.getElementById("excuse");
+  element.textContent = randomSentence;
 };
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
